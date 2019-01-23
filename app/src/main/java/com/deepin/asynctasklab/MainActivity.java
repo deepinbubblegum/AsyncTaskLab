6f46f21c8e2c4f1b95a55e3cb926bf69f7cf2174
deepinbubblegum <5911110222@mutacth.com>
2019-01-23 13:28:25 +0700

    commit

--- app/src/main/java/com/deepin/asynctasklab/MainActivity.java
+++ app/src/main/java/com/deepin/asynctasklab/MainActivity.java
@@ -3,16 +3,20 @@ package com.deepin.asynctasklab;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    ProgressBar mProgressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mProgressBar = findViewById(R.id.progressBar);
     }
 
     public void startTask(View view) {
+        new SimpleTask(mProgressBar).execute();
     }
 }
