c3267d24cb78b402b3819d5f64299d8180928188
deepinbubblegum <5911110222@mutacth.com>
2019-01-23 14:28:20 +0700

    add 2 progressbar

--- app/src/main/java/com/deepin/asynctasklab/MainActivity.java
+++ app/src/main/java/com/deepin/asynctasklab/MainActivity.java
@@ -8,19 +8,21 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ProgressBar mProgressBar;
-    TextView mTextView;
+    ProgressBar mProgressBar,mProgressBar2;
+    TextView mTextView,mTextView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mProgressBar = findViewById(R.id.progressBar);
+        mProgressBar2 = findViewById(R.id.progressBar2);
         mTextView = findViewById(R.id.display);
+        mTextView2 = findViewById(R.id.display2);
     }
 
     public void startTask(View view) {
 
-        new SimpleTask(mProgressBar,mTextView).execute();
+        new SimpleTask(mProgressBar,mProgressBar2,mTextView,mTextView2).execute();
     }
 }
--- app/src/main/java/com/deepin/asynctasklab/SimpleTask.java
+++ app/src/main/java/com/deepin/asynctasklab/SimpleTask.java
@@ -7,11 +7,13 @@ import android.widget.TextView;
 import java.lang.ref.WeakReference;
 
 public class SimpleTask extends AsyncTask <Void, Integer, Void> {
-    private WeakReference<ProgressBar> mProgressBar;
-    private WeakReference<TextView> mTextView;
-    SimpleTask(ProgressBar pb, TextView tv){
+    private WeakReference<ProgressBar> mProgressBar,mProgressBar2;
+    private WeakReference<TextView> mTextView,mTextView2;
+    SimpleTask(ProgressBar pb,ProgressBar pb2, TextView tv,TextView tv2){
         mProgressBar = new WeakReference<>(pb);
+        mProgressBar2 = new WeakReference<>(pb2);
         mTextView = new WeakReference<>(tv);
+        mTextView2 = new WeakReference<>(tv2);
     }
 
     @Override
@@ -24,12 +26,15 @@ public class SimpleTask extends AsyncTask <Void, Integer, Void> {
             }
             publishProgress(i);
         }
+
         return null;
     }
 
     @Override
     protected void onProgressUpdate(Integer... values) {
         mProgressBar.get().setProgress(values[0]);
+        mProgressBar2.get().setProgress(100 - values[0]);
         mTextView.get().setText(values[0]+" %");
+        mTextView2.get().setText(100 - values[0]+" %");
     }
 }
